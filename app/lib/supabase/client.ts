05e5f80310d7115a35df826c515c77b993a858c5
Michael Antoni <michaelantoni.tech@gmail.com>
2025-11-17 00:19:47 +0800

    revert back the subapase initial setup due to vercel build error

--- app/lib/supabase/client.ts
+++ app/lib/supabase/client.ts
@@ -1,9 +1,7 @@
-import { ENV } from '@/app/constants/env';
 import { createClient } from '@supabase/supabase-js';
 
-// URL and anon key from environment variables
-const SUPABASE_URI = ENV.SUPABASE_URI;
-const SUPABASE_ANON_KEY = ENV.SUPABASE_ANON_KEY;
-
 // Initialize the Supabase client
-export const supabase = createClient(SUPABASE_URI, SUPABASE_ANON_KEY);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URI!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+);
